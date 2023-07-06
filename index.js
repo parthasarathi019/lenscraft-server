8587af450885110ccf9caa4744608053d4817544
parthasarathi019 <parthasarathaie@gmail.com>
2023-07-06 18:41:23 +0600

    adding create user api

--- index.js
+++ index.js
@@ -188,7 +188,7 @@ const run = async () => {
     });
     
 
-    // approve or deny classs by admin
+    // approve or deny classsss by admin
     app.patch("/api/classes/:id", verifyJWT, verifyAdmin, async (req, res) => {
       try {
         const {action} = req.body;
